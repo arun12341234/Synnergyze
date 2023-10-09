72d43d4ba8854e78e748c057b09c9e24404985fd
rahul9561 <rahul8969051051@gmail.com>
2023-10-09 18:01:10 +0530

    news

--- src/Shopping/offer.jsx
+++ src/Shopping/offer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Offer = () => {
   return (
     <div style={{position:"fixed",left:"0",right:"0",top:"0", background:" #f8f8f8",zIndex: "22"}}>
-      <p style={{textAlign:"center",fontSize:"13px"}}>For any query,email us at <a href="/" style={{color:"red"}}>care@industrybuying.com </a> or call us on <i> </i>+91 4567899876</p>
+      <p style={{fontSize:"13px"}}>For any query,email us at <a href="/" style={{color:"red"}}>care@industrybuying.com </a> or call us on <i> </i>+91 4567899876</p>
       
     </div>
   )
